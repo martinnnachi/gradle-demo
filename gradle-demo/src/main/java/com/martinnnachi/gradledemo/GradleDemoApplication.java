51db787706c60ccae0aacf6e42e8874c44849b43
martinnnachi <mnnachi@gmail.com>
2019-01-30 19:35:43 +0000

    second commit

---
+++ gradle-demo/src/main/java/com/martinnnachi/gradledemo/GradleDemoApplication.java
@@ -0,0 +1,14 @@
+package com.martinnnachi.gradledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDemoApplication.class, args);
+	}
+
+}
+
